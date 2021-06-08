0d3eed826e089fe83f9ab5c2d66354b52906c17b
Madhumitha <mithalns@gmail.com>
2021-06-08 10:13:50 +0200

    initialising agora_voice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Agora from 'agora-rtc-sdk';
+export default function AgoraVoice() {
+  const [client, setClient] = useState(null);
+  const [uid, setUid] = useState(null);
+  const [localStream, setLocalStream] = useState(null);
+  const join = () => {
+    let chanalName = 'abc';
+    let clientInstance = Agora.createClient({ mode: 'rtc', codec: 'h264' });
 
-function App() {
+    clientInstance.init('6d76263dbe5f40339c641441bb6b7987', () => {
+      clientInstance.join(null, chanalName, null, (uid) => {
+        let localStreamInstance = Agora.createStream({
+          streamID: uid,
+          audio: true,
+          video: false,
+          screen: false,
+        });
+        setLocalStream(localStreamInstance);
+        localStreamInstance.init(() => {
+          clientInstance.publish(localStreamInstance);
+          localStreamInstance.play('local_stream');
+        });
+
+        clientInstance.on('stream-added', (evt) => {
+          let remoteStream = evt.stream;
+          const id = remoteStream.getId();
+          client.subscribed(remoteStream);
+        });
+
+        clientInstance.on('stream-subscribed', (evt) => {
+          let remoteStream = evt.stream;
+          remoteStream.play('remote_stream');
+        });
+      });
+    });
+    setClient(clientInstance);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={join}>JOIN CALL</button>
+      <div id='local_stream'></div>
+      <div id='remote_stream'></div>
     </div>
   );
 }
-
-export default App;
